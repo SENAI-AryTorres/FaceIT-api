152ee3e6b3fd0f92bb03fe6c43bb87832e701a5b
Brendon Pereira dos Santos <brendon@cobrape.com.br>
2020-03-06 15:19:32 -0300

    Criação da controller pessoaskill Criação da controller skill

---
+++ Controllers/SkillController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using faceitapi.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace faceitapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillController : ControllerBase
+    {
+        private readonly faceitContext faceitContext;
+
+        public SkillController()
+        {
+            faceitContext = new faceitContext();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllSkills()
+        {
+            try
+            {
+                var data = await faceitContext.Skill
+                .ToListAsync();
+
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
