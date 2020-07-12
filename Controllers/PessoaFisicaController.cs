3f03ec51ff1e21ae1732f69ba7587ac7986c12e8
BRENDON SANTOS <brendon.santos3@senaisp.edu.br>
2020-07-12 02:01:01 -0300

    Correção do insert da pessoa física

--- Controllers/PessoaFisicaController.cs
+++ Controllers/PessoaFisicaController.cs
@@ -93,11 +93,11 @@ namespace faceitapi.Controllers
                     {
                         await faceitContext.PessoaSkill.AddRangeAsync(model.IDPessoaNavigation.PessoaSkill);
                     }
-                    if (model.IDPessoaNavigation.Imagem.Bytes != null)
+                    if (model.IDPessoaNavigation.Imagem != null)
                     {
                         await faceitContext.Imagem.AddAsync(model.IDPessoaNavigation.Imagem);
                     }
-                    if (model.IDPessoaNavigation.Anexo.Bytes != null)
+                    if (model.IDPessoaNavigation.Anexo != null)
                     {
                         await faceitContext.Anexo.AddAsync(model.IDPessoaNavigation.Anexo);
                     }
